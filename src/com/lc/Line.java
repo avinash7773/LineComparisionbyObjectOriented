39505ceb0d34c5381cc5e7263f4fe60da7deb5a1
avinash7773 <javinash228@gmail.com>
2021-04-11 17:34:01 +0530

    Check equality of two length

--- src/com/lc/Line.java
+++ src/com/lc/Line.java
@@ -13,4 +13,26 @@ public class Line {
 		double length = Math.sqrt((p2.x - p1.y) * (p2.x - p1.y) + (p2.y - p1.y) * (p2.y - p1.y));
 		return length;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Line other = (Line) obj;
+		if (p1 == null) {
+			if (other.p1 != null)
+				return false;
+		} else if (!p1.equals(other.p1))
+			return false;
+		if (p2 == null) {
+			if (other.p2 != null)
+				return false;
+		} else if (!p2.equals(other.p2))
+			return false;
+		return true;
+	}
 }
--- src/com/lc/Main.java
+++ src/com/lc/Main.java
@@ -6,9 +6,22 @@ public class Main {
 		Point p1 = new Point(1,4);
 		Point p2 = new Point(7,8);
 		
-		Line length = new Line(p1, p2);
+		Point p3 =new Point(1, 4);
+		Point p4 =new Point(7, 6);
 		
-		System.out.println("Length of line="+length.getLength());
+		Line obj1 = new Line(p1, p2);
+		Line obj2 = new Line(p3, p4);
+		
+		Double length1 = obj1.getLength();
+		Double length2 = obj2.getLength();
+		
+		System.out.println("Length of line1="+length1);
+		System.out.println("Length of line2="+length2);
+		
+		if(length1.equals(length2)) 
+			System.out.println("Lengths are equal");
+		else
+			System.out.println("Length are not equal");
 	}
 
 }
